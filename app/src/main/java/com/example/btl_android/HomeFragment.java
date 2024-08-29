ca1316c6dd0683ab2eb17214a0f77f12758b9913
VuVietCuong <cuonghaiduongg2004@gmail.com>
2024-08-29 22:39:28 +0700

    Cuong

--- app/src/main/java/com/example/btl_android/HomeFragment.java
+++ app/src/main/java/com/example/btl_android/HomeFragment.java
@@ -3,10 +3,16 @@ package com.example.btl_android;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +20,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+    GridView gridView;
+    GridView gridView2;
+
+    ArrayList<Item> itemArrayList;
+    ArrayList<Item2> item2ArrayList;
+    MyAdapter adapter;
+    MyAdapter2 adapter2;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +66,68 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        gridView = view.findViewById(R.id.gridView);
+        gridView2 = view.findViewById(R.id.gridview2);
+
+
+        itemArrayList = new ArrayList<>();
+        Item i1 = new Item("Bai hat 1", "Ca si 1", R.drawable.bg1);
+        Item i2 = new Item("Bai hat 1", "Ca si 1", R.drawable.bg1);
+        Item i3 = new Item("Bai hat 1", "Ca si 1", R.drawable.bg1);
+        Item i4 = new Item("Bai hat 1", "Ca si 1", R.drawable.bg1);
+        Item i5 = new Item("Bai hat 1", "Ca si 1", R.drawable.bg1);
+        Item i6 = new Item("Bai hat 1", "Ca si 1", R.drawable.bg1);
+
+        item2ArrayList = new ArrayList<>();
+        Item2 it1 = new Item2("Bai hat 1", R.drawable.bg1);
+        Item2 it2 = new Item2("Bai hat 1", R.drawable.bg1);
+        Item2 it3 = new Item2("Bai hat 1", R.drawable.bg1);
+        Item2 it4 = new Item2("Bai hat 1", R.drawable.bg1);
+        Item2 it5 = new Item2("Bai hat 1", R.drawable.bg1);
+        Item2 it6 = new Item2("Bai hat 1", R.drawable.bg1);
+
+
+
+        itemArrayList.add(i1);
+        itemArrayList.add(i2);
+        itemArrayList.add(i3);
+        itemArrayList.add(i4);
+        itemArrayList.add(i5);
+        itemArrayList.add(i6);
+
+        item2ArrayList.add(it1);
+        item2ArrayList.add(it2);
+        item2ArrayList.add(it3);
+        item2ArrayList.add(it4);
+        item2ArrayList.add(it5);
+        item2ArrayList.add(it6);
+
+
+
+
+        adapter = new MyAdapter(itemArrayList, getContext());
+        gridView.setAdapter(adapter);
+
+
+        adapter2 = new MyAdapter2(item2ArrayList, getContext());
+        gridView2.setAdapter(adapter2);
+
+
+        return view;
+
+
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/btl_android/Item2.java
@@ -0,0 +1,28 @@
+package com.example.btl_android;
+
+public class Item2 {
+    private String tenBaiHat;
+    private int imageView;
+
+    public Item2(String tenBaiHat, int imageView) {
+        this.tenBaiHat = tenBaiHat;
+        this.imageView = imageView;
+    }
+
+    public String getTenBaiHat() {
+        return tenBaiHat;
+    }
+
+    public void setTenBaiHat(String tenBaiHat) {
+        this.tenBaiHat = tenBaiHat;
+    }
+
+    public int getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(int imageView) {
+        this.imageView = imageView;
+    }
+}
+
